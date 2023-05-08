ba247e466fc3fb0c76cf473fe60bf665c2d9beac
9th Symphony <bordadora67@gmail.com>
2023-05-08 23:02:02 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export function App() {
+  return (
+    <>
+    <Navbar>
+      
+    </Navbar>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function CollapsibleExample() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Bordadora test Web App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#login">Login</Nav.Link>
+            <NavDropdown title="More" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#report">Report</NavDropdown.Item>
+              <NavDropdown.Item href="#contact us">Contact Us</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Creator's Detail</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default CollapsibleExample;
