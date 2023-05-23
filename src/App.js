1922f89ecddb3861dd8c10875cfc00969514301d
Ryan Karlyle <bordadora67@gmail.com>
2023-05-23 09:04:38 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Navbar from './components/Navbar';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/Home'; 
+import About from './components/About';
 
 export function App() {
   return (
     <>
     <Navbar>
-      
+    <BrowserRouter>
+        <Routes>  
+          <Route index elemet = {<Home/>}/>
+          <Route path = "/Home" element = {<Home/>} />
+          <Route path = "About" element = {<About/>} />
+        </Routes>
+      </BrowserRouter>
     </Navbar>
     </>
   );
